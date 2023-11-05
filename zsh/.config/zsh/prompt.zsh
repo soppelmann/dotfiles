0495db6c1b32c9a074cee78823bfd6e41e366621
Getz Mikalsen <getz@smocke.df.lth.se>
2023-11-05 15:17:24 +0100

    add github branch to prompt

--- zsh/.config/zsh/prompt.zsh
+++ zsh/.config/zsh/prompt.zsh
@@ -1,10 +1,18 @@
+function parse_git_branch() {
+    git branch 2> /dev/null | sed -n -e 's/^\* \(.*\)/[\1]/p'
+}
+
+COLOR_DEF=$'%f'
+COLOR_GIT=$'%F{39}'
+setopt PROMPT_SUBST
+
 #PS1="%m:%~%(!.#.$) "                 # prompt
-PS1='%n@%m%D{} [%~]
+export PROMPT='%n@%m%D{} [%~] ${COLOR_GIT}$(parse_git_branch)${COLOR_DEF}
 -> '
 
 #linebreak after each command
-precmd() {
-    precmd() {
-        echo
-    }
-}
+#precmd() {
+#    precmd() {
+#        echo
+#    }
+#}
